d3096c86988594a657cbd075068ddce8b79afa3c
buseloff <buseloff@gmail.com>
2020-12-05 10:19:40 +0200

    First commit

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+"use strict";
+let x;
+let user = "Vasya";
+x = "Hello";
+let y, z;
+y = 5;
+alert("Hello!");
+console.log(user);
+console.log(x, y, z);
+x = "world";
+console.log(x);
+x = 100;
+console.log(x);
+console.log(x + y);
+x = "hello ";
+console.log(x + user);
+let _ = 100;
+let $ = 28.45;
+console.log($);
+let x1 = 110,
+  x2 = 200;
+
+console.log(x1 + " " + x2);
+x1 = x2;
+console.log(x1 + " " + x2);
+let x3 = "5000";
+x1 = x3;
+console.log(x1 + " " + x3);
+const MY_COLOR = "RED";
+MY_COLOR = "BLUE";
+console.log(MY_COLOR);
