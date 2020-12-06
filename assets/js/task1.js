e66c37d86a8fa9a43dfdbf4aa0ae60b44f1f2ae4
buseloff <buseloff@gmail.com>
2020-12-06 13:50:45 +0200

    Final commit

--- assets/js/task1.js
+++ assets/js/task1.js
@@ -20,7 +20,7 @@ if (x % 2 == 0 && x >= 100) {
   x = x * 3;
   alert(x);
 }*/
-let x;
+/*let x;
 let sum = 0;
 for (x = 2; x <= 100; x = x + 2) {
   sum += x;
@@ -54,4 +54,21 @@ number = parseInt(prompt("Enter number", 20));
 console.log("Table multiply");
 for (x = 0; x <= 9; x++) {
   console.log(`${x} * ${number} = ${number * x}`);
+}*/
+
+let i,
+  N = 10,
+  sum = 0;
+
+for (i = 1; i <= N; i++) {
+  console.log(`${1 / i}`);
+  sum += 1 / i;
 }
+console.log(sum);
+sum = 0;
+N = 30;
+for (i = 1; i <= N; i = i * 2) {
+  console.log(`${1 / i}`);
+  sum += 1 / i;
+}
+console.log(sum);
